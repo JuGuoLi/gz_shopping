ba81b763f386867b400ab7fa76b728d74d23d7b3
JuGuoLi <44639600+JuGuoLi@users.noreply.github.com>
2018-10-31 19:51:35 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const userRouter=require('./routers/user.js');
+const bodyParser=require('body-parser');
+var app=express();
+app.listen(3000);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
+app.use('/user',userRouter);
